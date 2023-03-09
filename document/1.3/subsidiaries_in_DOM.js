ac1d3cf958acfa5c50f878c86dd7748315fc3d1d
Kupparos <kupparos@gmail.com>
2023-03-09 15:08:50 +0700

    second_task

---
+++ document/1.3/subsidiaries_in_DOM.js
@@ -0,0 +1,13 @@
+<html>
+<body>
+  <div>Пользователи:</div>
+  <ul>
+    <li>Джон</li>
+    <li>Пит</li>
+  </ul>
+</body>
+</html>
+
+let div = document.body.firstElementChild
+let ul = document.body.children[1]
+let secondLi = document.body.lastElementChild.lastElementChild
