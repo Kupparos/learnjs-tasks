734fb13260cb0b4e5bcd7a4ee8adc15090d16111
Kupparos <kupparos@gmail.com>
2023-03-12 18:58:36 +0700

    second task

---
+++ network_requests/3.1/get_GitHub_user_data.js
@@ -0,0 +1,25 @@
+async function getUsers(names) {
+  let users = [];
+
+  for (let name of names) {
+    let user = fetch(`https://api.github.com/users/${name}`).then(
+      (successResponse) => {
+        if (successResponse.status != 200) {
+          return null;
+        } else {
+          return successResponse.json();
+        }
+      },
+      (failResponse) => {
+        return null;
+      }
+    );
+    users.push(user);
+  }
+
+  let result = await Promise.all(users);
+
+  return result;
+}
+
+getUsers(["kupparos", "octokit"]);
